9d16a498fe30ba1bb25e07d9775ef2a8671af870
Raphael Hoang <105470839+hqvjet@users.noreply.github.com>
2025-07-18 06:09:37 +0700

    fix (global): layout fixed (#20)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,9 +7,9 @@ const Layout = () => {
   return (
     <SidebarProvider>
       <DynamicSidebar />
-      <SidebarInset>
+      <SidebarInset className="flex flex-col w-full min-w-0">
         <Header />
-        <div className="flex flex-1 flex-col gap-4 p-4">
+        <div className="flex flex-1 flex-col gap-4 p-4 w-full min-w-0 overflow-hidden">
           <Outlet />
         </div>
       </SidebarInset>
